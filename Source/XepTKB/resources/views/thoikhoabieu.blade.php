6b0b3ca90b4d1d6ec15e874054aa7274e8c1f705
lyvan <lyvamax2018@gmail.com>
2017-12-29 19:32:32 +0700

    - Thêm thư viện chụp ảnh màn hình html2canvas. - Sử dụng thư viện chụp ảnh chụp thử bảng html tại trang thời khóa biểu.

--- Source/XepTKB/resources/views/thoikhoabieu.blade.php
+++ Source/XepTKB/resources/views/thoikhoabieu.blade.php
@@ -13,6 +13,56 @@
                 Trang thời khóa biểu
             </div>
         </div>
-    </div>    
+    </div>
+
+    <style>
+        .top-buffer { 
+            margin-top: 20px; 
+        }
+        .left-buffer { 
+            margin-left: 4%;
+        }
+    </style>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 top-buffer col-sm-offset-1" style="border: 1px solid red">
+                <img id="a" src="#" class="img-responsive" alt="Image">        
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="table-responsive" id="ddd">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>A</th>
+                    <th>A</th>
+                    <th>A</th>
+                    <th>A</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+
+    <script src="{{ asset('js/html2canvas.min.js') }}"></script>
+
+    <script>
+        html2canvas($("#ddd")[0]).then(function(canvas) {
+            $("#ddd-out").append(canvas);
+            $("#a").attr("src", canvas.toDataURL());
+        });
+    </script>
+
 
 @endsection
--- Source/XepTKB/routes/web.php
+++ Source/XepTKB/routes/web.php
@@ -13,5 +13,5 @@
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('doimatkhau');
+    return view('thoikhoabieu');
 });
