4a7e19a118e8793e8ac21bf78d74715c6bba5c36
lyvan <lyvamax2018@gmail.com>
2017-12-29 22:15:58 +0700

    - Xây dựng khung giao diện cho trang tạo tkb. - Chỉnh sửa dạng hiển thị thời khóa biểu và chụp ảnh thu nhỏ tkb trên trang ql tkb.

---
+++ Source/XepTKB/resources/views/taotkb.blade.php
@@ -0,0 +1,156 @@
+{{--  Mở rộng cho trang admin  --}}
+@extends('admin')
+
+{{--  Đặt title webpage  --}}
+@section('title', 'Xếp thời khóa biểu')
+
+{{--  Phần nội dung sẽ dẫn vào trang admin  --}}
+@section('noidung')
+    <div class="container">
+        
+        <div class="row">
+            
+            <div class="col-xs-12 col-sm-4 col-sm-push-8">
+                Học phần
+            </div>
+            <div class="col-xs-12 col-sm-8 col-sm-pull-4">
+                <h3 class="text-center">
+                    Thời khóa biểu minh họa
+                </h3>
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered table-condensed">
+                        <thead>
+                            <tr class="info">
+                                <th></th>
+                                <th>Thứ 2</th>
+                                <th>Thứ 3</th>
+                                <th>Thứ 4</th>
+                                <th>Thứ 5</th>
+                                <th>Thứ 6</th>
+                                <th>Thứ 7</th>                   
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"></td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"></td>
+                            </tr>
+                            <tr>
+                                <td>11</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>12</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        
+        
+    </div>   
+@endsection
--- Source/XepTKB/resources/views/thoikhoabieu.blade.php
+++ Source/XepTKB/resources/views/thoikhoabieu.blade.php
@@ -2,7 +2,7 @@
 @extends('admin')
 
 {{--  Đặt title webpage  --}}
-@section('title', 'Xếp thời khóa biểu')
+@section('title', 'Quản lý tkb')
 
 {{--  Phần nội dung sẽ dẫn vào trang admin  --}}
 @section('noidung')
@@ -15,6 +15,7 @@
         </div>
     </div>
 
+    {{--  Đặt margin giữa các thời khóa biểu thu nhỏ  --}}
     <style>
         .top-buffer { 
             margin-top: 20px; 
@@ -23,37 +24,160 @@
             margin-left: 4%;
         }
     </style>
+
+    {{--  Đặt màu cho cột thứ và tiết thời khóa biểu  --}}
+    <script>
+        $(document).ready(function () {
+            $('td:first-child').each(function() {
+                $(this).addClass("info");
+            });
+            $('th, td').addClass("text-center");
+        });
+    </script>
     
     <div class="container">
         <div class="row">
+            <a href="">
             <div class="col-xs-12 col-sm-3 top-buffer col-sm-offset-1" style="border: 1px solid red">
                 <img id="a" src="#" class="img-responsive" alt="Image">        
             </div>
+            </a>
         </div>
-    </div>
+    </div>    
 
-    
-    <div class="table-responsive" id="ddd">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>A</th>
-                    <th>A</th>
-                    <th>A</th>
-                    <th>A</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>a</td>
-                    <td>a</td>
-                    <td>a</td>
-                    <td>a</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    
+    <div class="container" id="ddd">
+        <div class="table-responsive">
+            <table class="table table-hover table-bordered table-condensed">
+                <thead>
+                    <tr class="info">
+                        <th></th>
+                        <th>Thứ 2</th>
+                        <th>Thứ 3</th>
+                        <th>Thứ 4</th>
+                        <th>Thứ 5</th>
+                        <th>Thứ 6</th>
+                        <th>Thứ 7</th>                   
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="7"></td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="7"></td>
+                    </tr>
+                    <tr>
+                        <td>11</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>12</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>    
 
     <script src="{{ asset('js/html2canvas.min.js') }}"></script>
 
